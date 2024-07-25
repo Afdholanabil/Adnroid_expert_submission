f529c6accc31489bf7943deeff40311f98f569b6
Afdholanabil <afdholanabil41435@gmail.com>
2024-07-25 11:30:37 +0700

    adding dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-
+        buildConfigField("String","API_KEY","\"8571e07c1c6347d19b31655f1c222d96\"")
     }
 
     buildTypes {
@@ -45,4 +45,20 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
+
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.11.0")
+    implementation ("com.squareup.retrofit2:adapter-rxjava2:2.9.0")
+    implementation ("com.squareup.okhttp3:okhttp:4.9.0")
+    implementation ("com.squareup.retrofit2:converter-scalars:2.9.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
+
+    implementation("androidx.exifinterface:exifinterface:1.3.7")
+
+    implementation("androidx.activity:activity-ktx:1.8.2")
+    implementation("androidx.fragment:fragment-ktx:1.6.2")
+
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.7.0")
 }
---
+++ app/src/main/java/com/example/submission_awal_android_expert/data/retrofit/ApiConfig.kt
@@ -0,0 +1,8 @@
+package com.example.submission_awal_android_expert.data.retrofit
+
+class ApiConfig {
+
+companion object {
+
+}
+}
