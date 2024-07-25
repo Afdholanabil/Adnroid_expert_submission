4d3f71f9a931055bcc7a21bc2ff3a895b3e42576
Afdholanabil <afdholanabil41435@gmail.com>
2024-07-25 11:06:50 +0700

    initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Submission-awal_android_expert"
+include(":app")
+ 
